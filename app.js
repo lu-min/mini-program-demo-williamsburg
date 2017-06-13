0467b3b1cc9b3bf5df405213c4040ba450e2597d
lu-min <lumin0116@gmail.com>
2017-06-13 13:08:04 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+//app.js
+App({
+  onLaunch: function () {
+    //Call API to get data from local storage
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //call the login API
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null
+  }
+})
